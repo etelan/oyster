8054270cd68dcce6bd7a9b2aed178170cfbdde21
Adam Baker <adamjamesbaker@gmail.com>
2020-10-19 14:37:41 +0100

    max

--- lib/card.rb
+++ lib/card.rb
@@ -1,12 +1,17 @@
 class Oyster
   attr_reader :balance
 
+  # Constants - Class
+  @@MAX_BALANCE = 90
+
+  # Set initial variables
   def initialize(bal = 0)
     @balance = bal
   end
 
+  # Top up Method
   def top_up(money)
-    @balance += money
+    @balance + money > @@MAX_BALANCE ? raise("Can't Top Up - Over Max £90") : @balance += money
     "Your balance is now: £#{self.balance}"
   end
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,15 +4,19 @@ RSpec.describe Oyster do
 
   describe "@Balance" do
     it "returns the balance" do
-      blue_card = Oyster.new
-      expect(blue_card.balance).to eq 0
+      expect(subject.balance).to eq 0
     end
   end
 
   describe "#top_up" do
     it "adds number to the balance" do
-      blue_card = Oyster.new
-      expect(blue_card.top_up(2)).to eq "Your balance is now: £#{blue_card.balance}"
+      expect(subject.top_up(2)).to eq "Your balance is now: £#{subject.balance}"
+    end
+  end
+
+  describe "#top_up" do
+    it "error if over max when trying to top up" do
+      expect { subject.top_up(91) }.to raise_error("Can't Top Up - Over Max £90")
     end
   end
 
